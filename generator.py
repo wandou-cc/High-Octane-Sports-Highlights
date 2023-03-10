4cabdc0a055b448df3058ed20706776732184708
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-03-10 09:03:33 +0000

    working on it

--- generator.py
+++ generator.py
@@ -4,4 +4,4 @@ import numpy as np
 import pandas as pd
 import IPython.display as ipd 
 import os,shutil
-from moviepy.video.io.ffmpeg_tools import ffmpeg_ex
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subc
