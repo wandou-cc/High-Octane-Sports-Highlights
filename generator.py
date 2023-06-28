32737ec44e81590503841c11c00b3fe1877e61a1
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-06-28 16:36:37 +0000

    Remove some debug output

--- generator.py
+++ generator.py
@@ -17,4 +17,5 @@ print(int(librosa.get_duration(vid, sample_rate)/60))
 chunk_size=5 
 window_length = chunk_size * sample_rate
 
-#seeing an audio sample and it's time-amplitude gra
+#seeing an audio sample and it's time-amplitude graph
+a=v
