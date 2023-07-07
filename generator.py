9372259d609d1411f65e18fc46bddb19b82691e4
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-07-07 18:57:56 +0000

    update documentations

--- generator.py
+++ generator.py
@@ -20,4 +20,4 @@ window_length = chunk_size * sample_rate
 #seeing an audio sample and it's time-amplitude graph
 a=vid[5*window_length:6*window_length] 
 ipd.Audio(a, rate=sample_rate)
-energy 
+energy = sum(a
