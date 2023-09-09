1eeb4d8c8ec1016940b80fe2e99615fee0a0d607
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-09-09 20:21:31 +0000

    Added robots.txt

--- generator.py
+++ generator.py
@@ -34,4 +34,4 @@ plt.hist(energy)
 plt.show()
 #Close graphs for progress of program
 
-#Finding and setting threshold value of commentator and audience noise above which we want to include port
+#Finding and setting threshold value of commentator and audience noise above which we want to include portion in h
