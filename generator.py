3e4fc689ac31e6c3707868efa0fe70d67dcacc11
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-06-11 09:04:54 +0000

    [MERGE] merge from dev3

--- generator.py
+++ generator.py
@@ -91,4 +91,5 @@ for i in range(len(df)):
 files=os.listdir(sub_folder)
 files=[sub_folder+"/highlight" + str(i+1) + ".mp4" for i in range(len(df))]
 #print(files)
-final_clip=concatenate_videoclips([VideoFileClip(i) for i in fi
+final_clip=concatenate_videoclips([VideoFileClip(i) for i in files])
+fi
