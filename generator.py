4dded38ac914559569262401fab8067059bc7165
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-06-27 06:45:25 +0000

    edit composer.json

--- generator.py
+++ generator.py
@@ -92,4 +92,4 @@ files=os.listdir(sub_folder)
 files=[sub_folder+"/highlight" + str(i+1) + ".mp4" for i in range(len(df))]
 #print(files)
 final_clip=concatenate_videoclips([VideoFileClip(i) for i in files])
-final_clip.write_videofile("KKRvsRCB_Highlights.mp4") #Enter the desired ou
+final_clip.write_videofile("KKRvsRCB_Highlights.mp4") #Enter the desired output high
